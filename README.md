3a19e22784746619a45ad0ac62258b81908dbeb0
trku <trevku155@gmail.com>
2018-05-30 14:23:33 -0400

    add some small files for experimentation

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Good Tutorial:
 http://socialmedia-class.org/twittertutorial.html
 
 
-## setup
+## local setup
 setup a virtual environment in python3:
 
 `virtualenv -p python3 venv`
@@ -33,4 +33,18 @@ point PyCharm to the virtualenv:
 
 - point it to /path-to-stupefaction-project/venv/bin/python
 
+### Requirements so far
+I'll be using:
+- tweepy
+- spaCy
 
+Make sure to install them in your venv.
+
+For spacy, have to download languages:
+
+`python -m spacy download en`
+
+## Operation
+so far my idea is:
+- use tweepy to get tweets, write to file
+- some other script reads these files, use spacy to analyze
--- main/spacy_sandbox.py
+++ main/spacy_sandbox.py
@@ -1,2 +1,34 @@
 import spacy
 
+
+def pos_tagging_example():
+    doc = nlp("Apple is looking at buying U.K. startup for $1 billion.")
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_)
+
+
+def noun_chunking_example():
+    doc = nlp("Autonomous cars shift insurance liability toward manufacturers")
+    for chunk in doc.noun_chunks:
+        print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+
+
+def named_entities_example():
+    doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+
+def sentence_segmentation_example():
+    doc = nlp("This is a sentence. This is another sentence.")
+    for sent in doc.sents:
+        print(sent.text)
+
+
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_sm")
+
+    # pos_tagging_example()
+    # noun_chunking_example()
+    # named_entities_example()
+    sentence_segmentation_example()
