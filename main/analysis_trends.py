5c27946b2405930f5eb9d190bc639be6072301bb
trku <trevku155@gmail.com>
2018-06-08 00:03:12 -0400

    small cosmetic changes

--- main/analysis_trends.py
+++ main/analysis_trends.py
@@ -19,49 +19,51 @@ import json
 import os
 
 
-def top_ten_all(trends_data, output_filepath):
+def top_ten_all(trends_data, num_trends, output_filepath):
     """
-    Write the top 10 trends for all woeids in trends_data to the output_filepath.
+    Write the top trends for all locations (woeids) in trends_data to the output_filepath.
 
     :param trends_data: list of dictionaries, containing trend data
+    :param num_trends: number of top trends to write out for each location
     :param output_filepath: string, output file name to write to
     """
     with open(output_filepath, "w") as f:
-        f.write("Top 10 Trends for locations in Canada\n\n")
+        f.write(str("Top %d Trends for locations in Canada\n\n" % num_trends))
 
         for location in trends_data:
-            f.write("Top 10 Trends for %s (woeid %d), on %s:\n" % (location["location_name"],
+            f.write("Top %d Trends for %s (woeid %d), on %s:\n" % (num_trends, location["location_name"],
                                                                    location["woeid"],
                                                                    location["starting"].split("T")[0]))
-            for i in range(10):
+            for i in range(num_trends):
                 f.write("%d. %s\n" % (i + 1, location["trend_list"][i]))
             f.write("\n")
 
 
-def unique_trending(trends_data, output_filepath):
+def unique_trending(trends_data, num_results, output_filepath):
     """
     Write unique trends for the woeids in trends_data. Write data to output_filepath.
 
     :param trends_data: list of dictionaries, containing trend data
+    :param num_results: number of top trending entries to consider
     :param output_filepath: string, output file name to write to
     """
     with open(output_filepath, "w") as f:
-        f.write("Unique Trending topics for locations in Canada\n\n")
+        f.write("Unique Top %d trending topics for locations in Canada\n\n" % num_results)
 
         for i in range(len(trends_data)):
             location = trends_data[i]
 
             # set of trending for this location
-            top_ten = set(location["trend_list"][:10])
+            top = set(location["trend_list"][:num_results])
 
             # set of trending for all other locations combined
-            others_top_ten = set()
+            others_top = set()
             for j in range(len(trends_data)):
                 if j != i:
-                    others_top_ten = others_top_ten.union(set(trends_data[j]["trend_list"][:10]))
+                    others_top = others_top.union(set(trends_data[j]["trend_list"][:num_results]))
 
             # get unique trends for this location
-            unique = top_ten.difference(others_top_ten)
+            unique = top.difference(others_top)
 
             # write out
             if len(unique) > 0:
@@ -73,41 +75,42 @@ def unique_trending(trends_data, output_filepath):
                     f.write(elem + "\n")
                 f.write("\n")
             else:
-                f.write("There were no unique trends in the top 10 trends for %s (woeid %d), on %s.\n\n" %
-                        (location["location_name"], location["woeid"], location["starting"].split("T")[0]))
+                f.write("There were no unique trends in the top %d trends for %s (woeid %d), on %s.\n\n" %
+                        (num_results, location["location_name"], location["woeid"], location["starting"].split("T")[0]))
 
 
-def common_trending(trends_data, output_filepath):
+def common_trending(trends_data, num_results, output_filepath):
     """
-    For each location in trends_data, get all the common top 10 trends.
+    For each location in trends_data, get all the common top num_results trends.
     Write data out to output_filepath.
 
     :param trends_data: list of dictionaries, containing trends data
+    :param num_results: number of top trending entries to consider
     :param output_filepath: string, output file to write to
     """
     with open(output_filepath, "w") as f:
-        f.write("Common Top 10 Trending topics for locations in Canada\n")
+        f.write("Common Top %d Trending topics for locations in Canada\n" % num_results)
         all_locations = list(map(lambda d: d["location_name"], trends_data))
-        f.write("All Locations: ")
+        f.write("All Locations Considered: ")
         for location in all_locations:
             f.write(location + ",")
         f.write("\n\n")
 
-        # intersect all top 10s
+        # intersect all top
         if len(trends_data) == 0:
             # shouldn't happen, but handle gracefully
             f.write("No data available!")
         elif len(trends_data) == 1:
-            top_ten = trends_data[0]["trend_list"][:10]
-            for t in top_ten:
-                f.write(t + " ")
+            top = trends_data[0]["trend_list"][:num_results]
+            for t in top:
+                f.write(t + "\n")
         else:
-            common_top_ten = set(trends_data[0]["trend_list"][:10])
+            common_top = set(trends_data[0]["trend_list"][:num_results])
             for i in range(1, len(trends_data)):
-                top_ten = set(trends_data[i]["trend_list"][:10])
-                common_top_ten = common_top_ten.intersection(top_ten)
-            for t in common_top_ten:
-                f.write(t + " ")
+                top = set(trends_data[i]["trend_list"][:num_results])
+                common_top = common_top.intersection(top)
+            for t in common_top:
+                f.write(t + "\n")
 
 
 if __name__ == "__main__":
@@ -119,16 +122,16 @@ if __name__ == "__main__":
 
     input_filepath = args.input
     output_dir = args.output
-    timestamp = input_filepath.split("-")[1]
+    timestamp = "-".join(input_filepath.split("-")[1:])
 
     with open(input_filepath, "r") as input_file:
         trends_data = list(map(lambda x: json.loads(x), input_file.readlines()))
 
         # get a report of the top 10 trends of these locations
-        top_ten_all(trends_data, os.path.join(output_dir, "trends-top10"))
+        top_ten_all(trends_data, 10, os.path.join(output_dir, "trends-top10" + "-" + timestamp))
 
-        # get a report of the unique trends in these locations
-        unique_trending(trends_data, os.path.join(output_dir, "trends-unique"))
+        # get a report of the unique trends from top 20 in these locations
+        unique_trending(trends_data, 20, os.path.join(output_dir, "trends-unique" + "-" + timestamp))
 
         # get a report of the common trends across locations
-        common_trending(trends_data, os.path.join(output_dir, "trends-common"))
+        common_trending(trends_data, 20, os.path.join(output_dir, "trends-common" + "-" + timestamp))
